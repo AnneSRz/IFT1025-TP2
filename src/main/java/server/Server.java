a38ee2263464c9aee7e68887e624278a2b8ef7f2
Anne Sophie Rozéfort <114533515+AnneSRz@users.noreply.github.com>
2023-03-23 00:16:14 -0400

    Tache 1 complete + begin javadoc

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -10,6 +10,19 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import server.models.Course;
+import java.io.File;
+import server.models.RegistrationForm;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+/**
+ * Le serveur peut éxécuter des commandes spécifiques tels que REGISTER_COMMAND et LOAD_COMMAND pour permettre a
+ * un client charger une liste de cours pour une session voulue et s'y inscrire.
+ * Cette classe permet de créer un serveur et ensuite de lire et écrire des objets entre le serveur et le client,
+ * tout en gérant les commandes qui sont envoyées par le client.
+ */
 public class Server {
 
     public final static String REGISTER_COMMAND = "INSCRIRE";
@@ -20,6 +33,14 @@ public class Server {
     private ObjectOutputStream objectOutputStream;
     private final ArrayList<EventHandler> handlers;
 
+    /**
+     * Ce constructeur en prenant un paramètre type int, crèe ue instance se refère à la classe server sur un port,
+     * ce port est donné et la limite d'attente de connexion est de 1, ça définit un eventHandler pour gérer les
+     * évènements sur le serveur
+     *
+     * @param port
+     * @throws IOException
+     */
     public Server(int port) throws IOException {
         this.server = new ServerSocket(port, 1);
         this.handlers = new ArrayList<EventHandler>();
@@ -90,8 +111,47 @@ public class Server {
      La méthode gère les exceptions si une erreur se produit lors de la lecture du fichier ou de l'écriture de l'objet dans le flux.
      @param arg la session pour laquelle on veut récupérer la liste des cours
      */
+    private ArrayList<Course> listeDeCours = new ArrayList<Course>();
     public void handleLoadCourses(String arg) {
-        // TODO: implémenter cette méthode
+        try{
+            //Step 1 : read cours.txt file ro get the information
+            File fichierTexte = new File("/src/main/Java/server/data/cours.txt");
+            Scanner cours = new Scanner(fichierTexte);
+            cours.useDelimiter("\t");
+
+            while (cours.hasNext()){
+                String nomCours = cours.next();
+                String sigle = cours.next();
+                String trimestre = cours.next();
+
+                Course coursDisponibles = new Course(nomCours, sigle, trimestre);
+                listeDeCours.add(coursDisponibles);
+                System.out.println(listeDeCours);
+            }
+
+            //Step 2 : Filtrer les cours selon la session donnée en arguments dans la fonction
+            ArrayList<Course> coursFiltres = new ArrayList<>();
+            for (Course lesCours : listeDeCours){
+                if (lesCours.getSession().equals(arg)){
+                    coursFiltres.add(lesCours);
+                }
+            }
+            System.out.println(coursFiltres);
+
+            //Step 3 : Retourner la liste d'onjet Courses au client via le socket en utilisant ObjectOutputStream
+            //pt pas necessaire puisqu'en executant serverauncher ça istancier server et l'executer sur le port 1337:
+            // ServerSocket ss = new ServerSocket(1337);
+            // Socket client = ss.accept();
+            ObjectOutputStream output = new ObjectOutputStream(client.getOutputStream());
+            output.writeObject(coursFiltres);
+
+            client.close();
+            output.close();
+
+        }catch (IOException ex){
+            ex.printStackTrace();
+            System.out.println("Erreur lors de la lecture du fichier");
+        }
     }
 
     /**
@@ -100,7 +160,36 @@ public class Server {
      La méthode gére les exceptions si une erreur se produit lors de la lecture de l'objet, l'écriture dans un fichier ou dans le flux de sortie.
      */
     public void handleRegistration() {
-        // TODO: implémenter cette méthode
+        try {
+            //Step 1 : Lire object registrationForm from Socket
+            //pt pas necessaire non plus puisqu'en executant serverauncher ça istancier server et l'executer sur le port 1337:
+            //ServerSocket ss = new ServerSocket(1337);
+            //Socket socket = ss.accept();
+
+            ObjectInputStream input = new ObjectInputStream(client.getInputStream());
+            RegistrationForm inscription = (RegistrationForm)input.readObject();
+            System.out.println(inscription);
+
+            //Step 2 : Save the object in Inscription.txt file (attention au format)
+            FileWriter fw = new FileWriter("inscription.txt");
+            BufferedWriter writer = new BufferedWriter(fw);
+
+            //Step 2.1 : Write object in inscription.txt
+            writer.write(inscription.getCourse().getSession() + "\t");
+            writer.write(inscription.getCourse().getCode() + "\t");
+            writer.write(inscription.getMatricule() + "\t");
+            writer.write(inscription.getPrenom()+ "\t");
+            writer.write(inscription.getNom() + "\t");
+            writer.write(inscription.getEmail() + "\t");
+
+            input.close();
+            fw.close();
+
+        }catch (IOException | ClassNotFoundException ex){
+            ex.printStackTrace();
+            System.out.println("Erreur lors de l'inscription");
+        }
+
     }
 }
 
