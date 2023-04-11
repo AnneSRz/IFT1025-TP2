026b05c71f0f6fa3cc98631c35c8d7cca46d24dd
Anne Sophie Rozéfort <114533515+AnneSRz@users.noreply.github.com>
2023-04-11 17:17:18 -0400

    Interface graphique

--- ClientFx/src/main/java/com/example/clientfx/Client_fx.java
+++ ClientFx/src/main/java/com/example/clientfx/Client_fx.java
@@ -6,23 +6,21 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Client_fx extends Application {
     private static Client_fxController controleur;
     @Override
     public void start(Stage stage) throws IOException {
-           // FXMLLoader fxmlLoader = new FXMLLoader(Client_fx.class.getResource("Client_fx.fxml"));
-            //Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-            stage.setTitle("Inscription UdeM");
-            VBox root = new VBox();
-            root.setSpacing(10);
-            root.setPadding(new Insets(10));
+        FXMLLoader fxmlLoader = new FXMLLoader(Client_fx.class.getResource("Client_fxView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 750, 500);
+        stage.setTitle("Inscription UdeM");
+        VBox root = new VBox();
+        root.setSpacing(10);
+        root.setPadding(new Insets(10));
 
-            Scene scene = new Scene(root, 900, 600);
-            stage.setScene(scene);
-            stage.show();
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- ClientFx/src/main/java/com/example/clientfx/Client_fxController.java
+++ ClientFx/src/main/java/com/example/clientfx/Client_fxController.java
@@ -1,26 +1,128 @@
 package com.example.clientfx;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import com.example.clientfx.modeleClientFx.Course;
 import com.example.clientfx.modeleClientFx.RegistrationForm;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 
 public class Client_fxController {
-    private Client_fxView vue;
     private Course module1;
     private RegistrationForm module2;
     @FXML
-    private Label chargerText;
+    private AnchorPane mainPane;
     @FXML
-    private Label envoyerText;
-
-    // Qu'est- ce qui apparait lorsqu'on clique sur un bouton par exemple
-    //Loraqu'on clique sur charger, la liste des cours filtres apparait
+    private TableView<Course> coursesDisplay;
     @FXML
-    protected void onChargerButtonClick() {chargerText.setText("Welcome to JavaFX Application!");
-    }
-    protected void onEnvoyerButtonClick() {envoyerText.setText("Welcome to JavaFX Application!");
+    private MenuButton coursLists;
+    @FXML
+    private Button chargementButton, registrer;
+    //@FXML private TextField prenomField, nomField, emailField, matriculeField;
+    //@FXML private Label emailError;
+    @FXML
+    private Label emailError, matriculeError;
+    @FXML
+    private TextField emailField, matriculeField;
+
+    public void initialize() {
+        TableColumn<Course, String> codeCol = new TableColumn<>("Code");
+        codeCol.setCellValueFactory(new PropertyValueFactory<>("code"));
+
+        TableColumn<Course, String> coursCol = new TableColumn<>("Cours");
+        coursCol.setCellValueFactory(new PropertyValueFactory<>("nom"));
+
+        coursesDisplay.getColumns().addAll(codeCol, coursCol);
+        //coursesDisplay.getItems().addAll(new Course("code1", "cours1"), new Course("code2", "cours2"));
     }
 
+    @FXML
+    public void handleCharge() {
+        System.out.println("Chargement des cours");
+    }
+
+    public void selection(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleRegistration() {
+        //ne pas oublier de traiter les exceptions si le fichiers de registration est mal formates.
+
+        // Step : Validder que l'email et le matricule  sont conformes avant d'envoyer le formulaire
+        boolean emailEstValide = validEmail(emailField, emailError, "Invalide, s'il vous plait réessayer");
+        boolean matriculeEstValide = validmatricule(matriculeField, matriculeError, "Invalide, s'il vous plait réessayer");
+        if (emailEstValide && matriculeEstValide) {
+            emailError.setText("Email Valide");
+            matriculeError.setText("Matricule Valide");
+        }
+        // Step : S'assurer que le matricule respecte le format avant d'envoyer le formulaire
+
+        System.out.println("Envoi des données du formulaire");
+    }
+
+    // Step : Valider le Email
+    private static boolean emailValidation(String text1) {
+        return text1.matches("^[\\w!#$%&'*+/=?`{|}~^.-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^.-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$");
+    }
+
+    private static boolean validEmail(TextField textField1, Label error, String msgErreur) {
+        boolean mail = emailValidation(textField1.getText());
+        if (!mail) {
+            textField1.getStyleClass().remove("invalide");
+           error.setText(msgErreur);
+           textField1.getStyleClass().add("invalide");
+        } else{
+            error.setText(null);
+        }
+        return mail;
+    }
+
+    // Step : Valider le matricule
+    private static boolean matriculeValidation(String text2) {
+        return text2.matches("^[0-9]{8}$");
+    }
+    private static boolean validmatricule(TextField textField2, Label erreur, String erreurMsg) {
+        boolean matricule = matriculeValidation(textField2.getText());
+        textField2.getStyleClass().remove("invalide");
+        if (!matricule) {
+            erreur.setText(erreurMsg);
+            textField2.getStyleClass().add("invalide");
+        } else {
+            erreur.setText(null);
+        }
+        return matricule;
+    }
 }
+
+
+
+// Ajouter les choix a la liste deroulante "choix de session"
+
+    // Action pour envoyer
+
+    /*
+    submitButton.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            if(nameField.getText().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(),
+                        "Form Error!", "Please enter your name");
+                return;
+            }
+            if(emailField.getText().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(),
+                        "Form Error!", "Please enter your email id");
+                return;
+            }
+            if(passwordField.getText().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(),
+                        "Form Error!", "Please enter a password");
+                return;
+            }
+
+            showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(),
+                    "Registration Successful!", "Welcome " + nameField.getText());
+        }
+    });
+
+     */
