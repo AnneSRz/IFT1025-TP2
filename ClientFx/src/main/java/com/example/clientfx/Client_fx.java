8f35346909776e4ff29654e515378caf9ff80704
Anne Sophie Rozéfort <114533515+AnneSRz@users.noreply.github.com>
2023-04-11 02:54:46 -0400

    Creation du client graphique 1.2

--- ClientFx/src/main/java/com/example/clientfx/Client_fx.java
+++ ClientFx/src/main/java/com/example/clientfx/Client_fx.java
@@ -2,19 +2,27 @@ package com.example.clientfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Client_fx extends Application {
+    private static Client_fxController controleur;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Client_fx.class.getResource("Client_fx.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+           // FXMLLoader fxmlLoader = new FXMLLoader(Client_fx.class.getResource("Client_fx.fxml"));
+            //Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+            stage.setTitle("Inscription UdeM");
+            VBox root = new VBox();
+            root.setSpacing(10);
+            root.setPadding(new Insets(10));
+
+            Scene scene = new Scene(root, 900, 600);
+            stage.setScene(scene);
+            stage.show();
     }
 
     public static void main(String[] args) {
--- ClientFx/src/main/java/com/example/clientfx/Client_fxController.java
+++ ClientFx/src/main/java/com/example/clientfx/Client_fxController.java
@@ -1,15 +1,26 @@
 package com.example.clientfx;
 
+import com.example.clientfx.modeleClientFx.Course;
+import com.example.clientfx.modeleClientFx.RegistrationForm;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
 public class Client_fxController {
+    private Client_fxView vue;
+    private Course module1;
+    private RegistrationForm module2;
     @FXML
-    private Label welcomeText;
+    private Label chargerText;
+    @FXML
+    private Label envoyerText;
 
     // Qu'est- ce qui apparait lorsqu'on clique sur un bouton par exemple
+    //Loraqu'on clique sur charger, la liste des cours filtres apparait
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onChargerButtonClick() {chargerText.setText("Welcome to JavaFX Application!");
     }
+    protected void onEnvoyerButtonClick() {envoyerText.setText("Welcome to JavaFX Application!");
+    }
+
 }
